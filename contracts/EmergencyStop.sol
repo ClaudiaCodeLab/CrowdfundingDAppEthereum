1d11efe7cfe66439da8d651286a6254b625a0aff
ClaudiaCodeLab <47373268+ClaudiaCodeLab@users.noreply.github.com>
2024-09-05 11:39:42 +0200

    Update EmergencyStop.sol

--- contracts/EmergencyStop.sol
+++ contracts/EmergencyStop.sol
@@ -5,23 +5,28 @@ import "./Ownable.sol";
 
 /// @title EmergencyStop Contract
 /// @dev Implements emergency stop functionality
+/// @author Claudia J.H.
 contract EmergencyStop is Ownable {
     bool public isStopped = false;
 
+    /// @notice Modifier to allow function calls only when the contract is not stopped
     modifier stoppedInEmergency {
         require(!isStopped, "Contract is stopped due to an emergency");
         _;
     }
 
+    /// @notice Modifier to allow function calls only when the contract is stopped
     modifier onlyWhenStopped {
         require(isStopped, "Contract is not stopped");
         _;
     }
 
+    /// @notice Allows the owner to stop the contract during an emergency
     function stopContract() public onlyAuthorized {
         isStopped = true;
     }
 
+    /// @notice Allows the owner to resume the contract after it has been stopped
     function resumeContract() public onlyAuthorized onlyWhenStopped {
         isStopped = false;
     }
