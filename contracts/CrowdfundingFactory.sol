1f856f088c054becdf6b67af376aa773a33c56ba
ClaudiaCodeLab <47373268+ClaudiaCodeLab@users.noreply.github.com>
2024-09-05 11:41:07 +0200

    Update CrowdfundingFactory.sol

--- contracts/CrowdfundingFactory.sol
+++ contracts/CrowdfundingFactory.sol
@@ -7,13 +7,20 @@ import "./Crowdfunding.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 /// @title CrowdfundingFactory Contract
-/// @dev Handles the creation and management of crowdfunding projects
+/// @dev Manages the creation and management of crowdfunding projects
+/// @author Claudia J.H. 
 contract CrowdfundingFactory is EmergencyStop, EternalStorage {
     using SafeMath for uint;
 
     Crowdfunding[] private projects;
     address payable _powner;
 
+    /// @notice Emitted when a new crowdfunding project is started
+    /// @param contractAddress The address of the new crowdfunding contract
+    /// @param projectStarter The address of the project creator
+    /// @param projectTitle The title of the project
+    /// @param deadline The deadline for the project
+    /// @param goalAmount The funding goal for the project
     event ProjectStarted(
         address contractAddress,
         address projectStarter,
@@ -22,10 +29,15 @@ contract CrowdfundingFactory is EmergencyStop, EternalStorage {
         uint goalAmount
     );
 
+    /// @notice Initializes the factory contract and sets the owner
     constructor() public payable {
         _powner = msg.sender;
     }
 
+    /// @notice Creates a new crowdfunding project
+    /// @param title The title of the crowdfunding project
+    /// @param durationInDays The duration of the fundraising campaign in days
+    /// @param amountToRaise The funding goal for the project (in Ether)
     function startProject(
         string calldata title,
         uint durationInDays,
@@ -50,6 +62,8 @@ contract CrowdfundingFactory is EmergencyStop, EternalStorage {
         );
     }
 
+    /// @notice Returns all active crowdfunding projects
+    /// @return Crowdfunding[] Array of all crowdfunding projects created
     function returnAllProjects() external view returns (Crowdfunding[] memory) {
         return projects;
     }
