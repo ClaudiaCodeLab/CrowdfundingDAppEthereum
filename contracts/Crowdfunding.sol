d48d3f509f3bce8bd237b21d915dd8bd36b0d839
ClaudiaCodeLab <47373268+ClaudiaCodeLab@users.noreply.github.com>
2024-09-05 11:44:19 +0200

    Update Crowdfunding.sol

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -8,7 +8,7 @@ import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/Saf
 
 /// @title Crowdfunding Contract
 /// @dev Manages individual crowdfunding campaigns
-/// @author Claudia J.H.  
+/// @author Claudia J.H. 
 contract Crowdfunding is EmergencyStop, EternalStorage, ReentrancyGuard {
     using SafeMath for uint256;
 
@@ -24,19 +24,34 @@ contract Crowdfunding is EmergencyStop, EternalStorage, ReentrancyGuard {
 
     mapping(address => uint) public contributions;
 
+    /// @notice Emitted when a contribution is received
+    /// @param contributor The address of the contributor
+    /// @param amount The amount contributed
+    /// @param currentTotal The current total amount of funds raised
     event FundingReceived(address contributor, uint amount, uint currentTotal);
+
+    /// @notice Emitted when the project creator is paid
+    /// @param recipient The address of the project creator
     event CreatorPaid(address recipient);
 
+    /// @notice Modifier to ensure the function is called only in a specific project state
+    /// @param _state The required state for the function to execute
     modifier inState(State _state) {
         require(state == _state, "Invalid project state");
         _;
     }
 
+    /// @notice Modifier to ensure only the project creator can call the function
     modifier isCreator() {
         require(msg.sender == creator, "Caller is not the project creator");
         _;
     }
 
+    /// @notice Initializes the crowdfunding contract with the given parameters
+    /// @param projectStarter The address of the project creator
+    /// @param projectTitle The title of the crowdfunding project
+    /// @param fundRaisingDeadline The deadline for the fundraising (in Unix time)
+    /// @param goalAmount The funding goal for the project (in Wei)
     constructor(
         address payable projectStarter,
         string memory projectTitle,
@@ -50,6 +65,7 @@ contract Crowdfunding is EmergencyStop, EternalStorage, ReentrancyGuard {
         currentBalance = 0;
     }
 
+    /// @notice Allows users to contribute to the crowdfunding project
     function contribute() external inState(State.FundRaising) payable stoppedInEmergency {
         require(msg.sender != creator, "Creator cannot contribute");
         contributions[msg.sender] = contributions[msg.sender].add(msg.value);
@@ -60,6 +76,7 @@ contract Crowdfunding is EmergencyStop, EternalStorage, ReentrancyGuard {
         checkIfFundingCompleteOrExpired();
     }
 
+    /// @notice Checks if the funding goal is reached or if the project has expired
     function checkIfFundingCompleteOrExpired() public {
         if (currentBalance >= amountGoal) {
             state = State.Successful;
@@ -70,6 +87,8 @@ contract Crowdfunding is EmergencyStop, EternalStorage, ReentrancyGuard {
         completeAt = block.timestamp;
     }
 
+    /// @notice Pays out the raised funds to the project creator if the goal is met
+    /// @return bool Returns true if the payout was successful
     function payOut() internal inState(State.Successful) nonReentrant returns (bool) {
         uint totalRaised = currentBalance;
         currentBalance = 0;
@@ -84,6 +103,8 @@ contract Crowdfunding is EmergencyStop, EternalStorage, ReentrancyGuard {
         return false;
     }
 
+    /// @notice Allows contributors to get a refund if the project expired without meeting the goal
+    /// @return bool Returns true if the refund was successful
     function getRefund() public inState(State.Expired) nonReentrant returns (bool) {
         require(contributions[msg.sender] != 0, "No contributions to refund");
 
